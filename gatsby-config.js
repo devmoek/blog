5fcb45cb9be97d895d3e0f2a81519d716cb8f531
moek <moekdeveloper@gmail.com>
2022-01-29 20:32:17 +0600

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      "trackingId": ""
+      "trackingId": "UA-201774409-1"
     }
   }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
